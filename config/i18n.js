e44dee451708fd8ae3dc2257deced128f7a2d60e
Paul Haddou <paul2405.63@hotmail.fr>
2023-05-04 10:16:46 +0200

    Initial commit to Vue x Vite project

--- config/i18n.js
+++
@@ -1,9 +0,0 @@
-import en from '../locales/en.json'
-import fr from '../locales/fr.json'
-
-export default {
-  defaultLocale: 'fr',
-  detectBrowserLanguage: false,
-  fallbackLocale: 'en',
-  messages: { en, fr }
-}
